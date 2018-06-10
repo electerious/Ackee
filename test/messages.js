5152c6895eba8a2ee2fd376c792b4560904bc7cb
Tobias Reich <tobias.reich.ich@gmail.com>
2018-06-10 14:48:05 +0200

    More tests

---
+++ test/messages.js
@@ -0,0 +1,17 @@
+'use strict'
+
+const test = require('ava')
+const uuid = require('uuid/v4')
+
+const messages = require('../src/messages')
+
+test('should extract messages from an object with errors', (t) => {
+
+	const message = uuid()
+	const errors = { 0: new Error(message) }
+
+	const result = messages(errors)
+
+	t.is(result, message)
+
+})
