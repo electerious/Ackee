970cd5b6aff69b8f9f9db03883f17640220cff9e
Tobias Reich <tobias.reich.ich@gmail.com>
2018-06-10 13:59:29 +0200

    First test

---
+++ test/ttl.js
@@ -0,0 +1,55 @@
+'use strict'
+
+const test = require('ava')
+
+const ttl = require('../src/ttl')
+
+test('return true when `timestamp` is valid', (t) => {
+
+	const date = new Date()
+	const timestamp = date.getTime()
+
+	const result = ttl(timestamp)
+
+	t.true(result)
+
+})
+
+test('return false when `timestamp` is invalid', (t) => {
+
+	const date = new Date()
+
+	date.setDate(date.getDate() - 1)
+
+	const timestamp = date.getTime()
+
+	const result = ttl(timestamp)
+
+	t.false(result)
+
+})
+
+test('return true when `timestamp` is newer than `ttl`', (t) => {
+
+	const date = new Date()
+	const timestamp = date.getTime()
+
+	const result = ttl(timestamp, 86400000)
+
+	t.true(result)
+
+})
+
+test('return false when `timestamp` is older than `ttl`', (t) => {
+
+	const date = new Date()
+
+	date.setDate(date.getDate() - 1)
+
+	const timestamp = date.getTime()
+
+	const result = ttl(timestamp, 0)
+
+	t.false(result)
+
+})
