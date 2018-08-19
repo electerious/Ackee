041be96e75f9e4f3447095e2fe9f38dacb784f3c
Tobias Reich <tobias.reich.ich@gmail.com>
2018-08-19 14:21:38 +0200

    Simplify header

--- src/ui/scripts/components/Header.js
+++ src/ui/scripts/components/Header.js
@@ -1,8 +1,6 @@
 import { createElement as h } from 'react'
 import { compose, setDisplayName } from 'recompose'
-
-import HeaderLogo from './HeaderLogo'
-import HeaderButton from './HeaderButton'
+import classNames from 'classnames'
 
 const enhance = compose(
 
@@ -10,14 +8,34 @@ const enhance = compose(
 
 )
 
+const Logo = () => (
+
+	h('div', { className: 'header__logo' })
+
+)
+
+const Button = (props) => (
+
+	h('button', {
+		className: classNames({
+			'header__button': true,
+			'active': props.active === true,
+			'link': true,
+			'color-white': props.active === true
+		}),
+		onClick: props.onClick
+	}, props.children)
+
+)
+
 const Component = (props) => (
 
 	h('header', { className: 'header' },
-		h(HeaderLogo),
+		h(Logo),
 		h('nav', {},
-			h(HeaderButton, { onClick: props.setRouteTab.bind(null, 'overview'), active: props.route.tab === 'overview' }, 'Overview'),
-			h(HeaderButton, { onClick: props.setRouteTab.bind(null, 'sites'), active: props.route.tab === 'sites' }, 'Sites'),
-			h(HeaderButton, { onClick: props.setRouteTab.bind(null, 'settings'), active: props.route.tab === 'settings' }, 'Settings')
+			h(Button, { onClick: props.setRouteTab.bind(null, 'overview'), active: props.route.tab === 'overview' }, 'Overview'),
+			h(Button, { onClick: props.setRouteTab.bind(null, 'sites'), active: props.route.tab === 'sites' }, 'Sites'),
+			h(Button, { onClick: props.setRouteTab.bind(null, 'settings'), active: props.route.tab === 'settings' }, 'Settings')
 		)
 	)
 
