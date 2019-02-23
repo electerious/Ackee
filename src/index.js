bfe26ebaa66d25b5ac7b6318e74d6deb63e2fd01
Tobias Reich <tobias.reich.ich@gmail.com>
2019-02-23 21:42:13 +0100

    Wait for callbacks

--- src/index.js
+++ src/index.js
@@ -10,9 +10,26 @@ const signale = require('./signale')
 const port = process.env.PORT || 3000
 const url = `http://localhost:${ port }`
 
-signale.start(`Starting the server`)
+server.on('listening', () => signale.watch(`Listening on ${ url }`))
+server.on('error', (err) => signale.fatal(err))
 
-mongoose.connect(process.env.MONGODB, { useNewUrlParser: true })
-server.listen(port)
+signale.await(`Connecting to ${ process.env.MONGODB }`)
 
-signale.watch(`Listening on ${ url }`)
+mongoose.connect(process.env.MONGODB, {
+
+	useNewUrlParser: true,
+	reconnectTries: Number.MAX_VALUE,
+	reconnectInterval: 1000
+
+}).then(() => {
+
+	signale.success(`Connected to ${ process.env.MONGODB }`)
+	signale.start(`Starting the server`)
+
+	server.listen(port)
+
+}).catch((err) => {
+
+	signale.fatal(err)
+
+})
