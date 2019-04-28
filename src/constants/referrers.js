e9cffe56eee7d4e156533f549b47f2d743e68a75
Tobias Reich <tobias.reich.ich@gmail.com>
2019-04-28 20:52:11 +0200

    Referrers const adjustments

--- src/constants/referrers.js
+++ src/constants/referrers.js
@@ -1,5 +1,5 @@
-// Must be strings instead of symbols, because they are used as
-// values in the DOM and in the URL of the referrer calls.
+// Constants will be shared between client and server.
+// They will be used as values in the DOM and in the URL of the referrer calls.
 const REFERRERS_SORTING_TOP = 'top'
 const REFERRERS_SORTING_RECENT = 'recent'
 
