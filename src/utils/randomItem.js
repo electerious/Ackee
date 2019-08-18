151e81290f28d80778d4b928baf50e376bdef0d0
Tobias Reich <tobias.reich.ich@gmail.com>
2019-08-18 15:21:01 +0200

    Function to get random item

---
+++ src/utils/randomItem.js
@@ -0,0 +1,3 @@
+'use strict'
+
+module.exports = (arr) => arr[Math.floor(Math.random() * arr.length)]
---
+++ test/utils/randomItem.js
@@ -0,0 +1,30 @@
+'use strict'
+
+const test = require('ava')
+const uuid = require('uuid/v4')
+
+const randomItem = require('../../src/utils/randomItem')
+
+test('return the only item', async (t) => {
+
+	const arr = [ uuid() ]
+
+	const result = randomItem(arr)
+
+	t.is(result, arr[0])
+
+})
+
+test('return random item', async (t) => {
+
+	const arr = [
+		uuid(),
+		uuid(),
+		uuid()
+	]
+
+	const result = randomItem(arr)
+
+	t.true(arr.includes(result))
+
+})
