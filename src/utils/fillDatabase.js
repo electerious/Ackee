3e3b21f67b10d02dc201f08f1e6b2f7bcf1580c3
Tobias Reich <tobias.reich.ich@gmail.com>
2019-09-06 21:19:25 +0200

    Try to randomize demo date even more

--- src/utils/fillDatabase.js
+++ src/utils/fillDatabase.js
@@ -10,13 +10,13 @@ const randomItem = require('./randomItem')
 const userAgents = Array(200).fill().map((_, index) => index)
 
 const weekdayDuds = [
-	[ null, null, null, null ], // Sunday
+	[ null, null, null, null, null, null, null, null ], // Sunday
 	[ null, null ], // Monday
 	[ null ], // Tuesday
-	[ null ], // Wednesday
+	[ null, null ], // Wednesday
 	[ null ], // Thursday
-	[ null, null ], // Friday
-	[ null, null, null ] // Saturday
+	[ null, null, null, null ], // Friday
+	[ null, null, null, null, null, null ] // Saturday
 ]
 
 const referrers = [
@@ -30,6 +30,7 @@ const referrers = [
 	'https://github.com/electerious',
 	'https://facebook.com/AppsLaudable',
 	'https://twitter.com/electerious',
+	'https://producthunt.com',
 	'https://google.com',
 	'https://bing.com'
 ]
