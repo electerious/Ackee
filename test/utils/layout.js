e6bca0699d951f4e0c75bc20674e1330cf6d8afb
Tobias Reich <tobias.reich.ich@gmail.com>
2019-09-07 15:21:23 +0200

    Test layout

---
+++ test/utils/layout.js
@@ -0,0 +1,35 @@
+'use strict'
+
+const test = require('ava')
+const uuid = require('uuid/v4')
+
+const layout = require('../../src/utils/layout')
+
+test('return HTML with body', async (t) => {
+
+	const body = uuid()
+	const result = layout(body, [], [])
+
+	t.true(result.includes(body))
+
+})
+
+test('return HTML with styles', async (t) => {
+
+	const styles = [ uuid(), uuid() ]
+	const result = layout('', styles, [])
+
+	t.true(result.includes(styles[0]))
+	t.true(result.includes(styles[1]))
+
+})
+
+test('return HTML with scripts', async (t) => {
+
+	const scripts = [ uuid(), uuid() ]
+	const result = layout('', [], scripts)
+
+	t.true(result.includes(scripts[0]))
+	t.true(result.includes(scripts[1]))
+
+})
