0464136d5275a2ecc57401cfa03984a6c923af7d
Tobias Reich <tobias.reich.ich@gmail.com>
2019-09-10 21:14:46 +0200

    New look for counter lists

--- src/ui/scripts/components/presentations/PresentationCounterList.js
+++ src/ui/scripts/components/presentations/PresentationCounterList.js
@@ -1,24 +1,26 @@
-import { createElement as h } from 'react'
+import { createElement as h, Fragment } from 'react'
 import PropTypes from 'prop-types'
-
-import Counter from '../Counter'
 
 import sumByProp from '../../utils/sumByProp'
 
 const Row = (props) => {
 
 	return (
-		h('a', {
-			className: 'itemList__row',
-			href: props.url.href,
-			target: '_blank'
-		},
-			h('div', {
-				className: 'itemList__data'
+		h(Fragment, {},
+			h('a', {
+				className: 'gridList__column gridList__column--leading',
+				href: props.url.href,
+				target: '_blank'
 			},
-				h('div', { className: 'itemList__bar', style: { '--width': `${ props.barWidth }%` } }),
-				h(Counter, {}, props.count),
-				h('span', { className: 'itemList__text' }, props.url.href)
+				h('div', { className: 'gridList__bar', style: { '--width': `${ props.barWidth }%` } }),
+				h('div', { className: 'color-main' }, `${ props.count }x`)
+			),
+			h('a', {
+				className: 'gridList__column',
+				href: props.url.href,
+				target: '_blank'
+			},
+				h('div', { className: 'gridList__truncated' }, props.url.href)
 			)
 		)
 	)
@@ -31,15 +33,17 @@ const PresentationCounterList = (props) => {
 	const proportionalWidth = ({ count }) => (count / totalCount) * 100
 
 	return (
-		h('div', { className: 'itemList' },
-			h('div', { className: 'itemList__inner' },
-				props.items.map((item, index) => (
-					h(Row, {
-						key: item.url.href + index,
-						barWidth: proportionalWidth(item),
-						...item
-					})
-				))
+		h('div', { className: 'gridList' },
+			h('div', { className: 'gridList__inner' },
+				h('div', { className: 'gridList__grid' },
+					props.items.map((item, index) => (
+						h(Row, {
+							key: item.url.href + index,
+							barWidth: proportionalWidth(item),
+							...item
+						})
+					))
+				)
 			)
 		)
 	)
