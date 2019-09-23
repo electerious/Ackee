b6cf9fb01d5b45ed9ddb908716f60a258237f0e0
Apollinaire <no-reply@apollinai.re>
2019-09-23 23:42:47 +0200

    Better function

--- src/utils/stripMongoSecrets.js
+++ src/utils/stripMongoSecrets.js
@@ -1,9 +1,12 @@
 'use strict'
 
 module.exports = (connectionUri) => {
-  if (connectionUri.indexOf('@') > -1 && connectionUri.indexOf('mongodb://' === 0)) {
+  if (typeof connectionUri === 'string' &&
+    connectionUri.indexOf('@') > -1 &&
+    connectionUri.indexOf('://') > -1) {
     const atIndex = connectionUri.indexOf('@')
-    return 'mongodb://' + connectionUri.slice(atIndex + 1)
+    const protocolIndex = connectionUri.indexOf('://')
+    return connectionUri.slice(0, protocolIndex + 3) + connectionUri.slice(atIndex + 1)
   } else {
     return connectionUri
   }
