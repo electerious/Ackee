01a5f56f78f26492a80b261a3c0c786b053a2494
Tobias Reich <tobias.reich.ich@gmail.com>
2019-10-06 13:57:19 +0200

    Use index for active element and update duration format

--- src/ui/scripts/components/presentations/PresentationValuesBar.js
+++ src/ui/scripts/components/presentations/PresentationValuesBar.js
@@ -1,4 +1,4 @@
-import { createElement as h, useState } from 'react'
+import { createElement as h, Fragment, useState } from 'react'
 import PropTypes from 'prop-types'
 
 import {
@@ -22,40 +22,57 @@ const Value = (props) => {
 
 }
 
+const Content = (props) => {
+
+	return (
+		h('div', { className: 'valuesBar__content' },
+			h('p', { className: `valuesBar__duration color-${ props.color }` },
+				props.aboveLimit === true && h('span', { className: 'valuesBar__decorator' }, '> '),
+				props.duration.value,
+				h('span', { className: 'valuesBar__unit' }, props.duration.unit)
+			),
+			h('p', { className: 'valuesBar__description' }, props.description)
+		)
+	)
+
+}
+
 const PresentationValuesBar = (props) => {
 
+	// Index of the active element
 	const [ active, setActive ] = useState()
 
-	const onEnter = (item) => setActive(item)
+	const onEnter = (index) => setActive(index)
 	const onLeave = () => setActive()
 
 	const averageDuration = props.items[0].average
 
-	const minDuration = props.items.reduce(minByProp('duration'), Number.MAX_SAFE_INTEGER)
-	const maxDuration = props.items.reduce(maxByProp('duration'), 0)
-
 	const belowLimitItems = props.items.filter(({ duration }) => duration < DURATIONS_LIMIT)
 	const aboveLimitItems = props.items.filter(({ duration }) => duration >= DURATIONS_LIMIT)
+
+	const minDuration = belowLimitItems.reduce(minByProp('duration'), Number.MAX_SAFE_INTEGER)
+	const maxDuration = belowLimitItems.reduce(maxByProp('duration'), 0)
 
 	const maxCount = props.items.reduce(maxByProp('count'), 0)
 	const proportionalOpacity = ({ count }) => (count / maxCount)
 
 	const content = (() => {
 
-		if (active == null) return h('div', { className: 'valuesBar__content' },
-			h('p', { className: 'valuesBar__duration color-white' }, formatDuration(averageDuration)),
-			h('p', { className: 'valuesBar__description' }, 'Average visit duration')
-		)
+		const activeItem = props.items[active]
 
-		const isAboveLimit = active.duration >= DURATIONS_LIMIT
+		if (activeItem == null) return h(Content, {
+			aboveLimit: false,
+			color: 'white',
+			duration: formatDuration(averageDuration),
+			description: 'Average visit duration'
+		})
 
-		const duration = `${ isAboveLimit === true ? '> ' : '' }${ formatDuration(active.duration) }`
-		const description = `${ active.count } total visits`
-
-		return h('div', { className: 'valuesBar__content' },
-			h('p', { className: 'valuesBar__duration color-primary' }, duration),
-			h('p', { className: 'valuesBar__description' }, description)
-		)
+		return h(Content, {
+			aboveLimit: activeItem.duration >= DURATIONS_LIMIT,
+			color: 'primary',
+			duration: formatDuration(activeItem.duration),
+			description: `${ activeItem.count } total visits`
+		})
 
 	})()
 
@@ -67,8 +84,8 @@ const PresentationValuesBar = (props) => {
 						h(Value, {
 							key: String(item.duration) + index,
 							opacity: proportionalOpacity(item),
-							onEnter: () => onEnter(item),
-							onLeave: () => onLeave(item),
+							onEnter: () => onEnter(index),
+							onLeave: () => onLeave(index),
 							...item
 						})
 					))
@@ -76,13 +93,13 @@ const PresentationValuesBar = (props) => {
 				h('div', { className: 'valuesBar__inner' },
 					h('div', { className: 'valuesBar__line' },
 						h('div', { className: 'valuesBar__anchor valuesBar__anchor--bottom' },
-							formatDuration(minDuration)
+							formatDuration(minDuration).toString()
 						)
 					),
 					content,
 					h('div', { className: 'valuesBar__line' },
 						h('div', { className: 'valuesBar__anchor valuesBar__anchor--top' },
-							formatDuration(maxDuration)
+							formatDuration(maxDuration).toString()
 						)
 					)
 				)
@@ -93,8 +110,8 @@ const PresentationValuesBar = (props) => {
 						h(Value, {
 							key: String(item.duration) + index,
 							opacity: proportionalOpacity(item),
-							onEnter: () => onEnter(item),
-							onLeave: () => onLeave(item),
+							onEnter: () => onEnter(belowLimitItems.length + index),
+							onLeave: () => onLeave(belowLimitItems.length + index),
 							...item
 						})
 					))
--- src/ui/scripts/utils/formatDuration.js
+++ src/ui/scripts/utils/formatDuration.js
@@ -1,5 +1,5 @@
 const removeLeadingZero = (str) => str.replace(/^0/, '')
-const removeTrailingZeros = (str) => str.replace(/:00$/, '')
+const removeTrailingZeros = (str) => str.replace(/(:00:00|:00)$/, '')
 const cleanZeros = (str) => removeLeadingZero(removeTrailingZeros(str))
 
 export default (ms) => {
@@ -8,11 +8,24 @@ export default (ms) => {
 	const duration = date.toISOString().substr(11, 8)
 
 	const hasMinutes = duration.match(/^00:00:/) == null
-	if (hasMinutes === false) return `${ cleanZeros(duration.substr(6)) }sec`
-
 	const hasHours = duration.match(/^00:/) == null
-	if (hasHours === false) return `${ cleanZeros(duration.substr(3)) }min`
 
-	return `${ cleanZeros(duration) }hrs`
+	if (hasMinutes === false) return {
+		toString: () => `${ cleanZeros(duration.substr(6)) }s`,
+		value: cleanZeros(duration.substr(6)),
+		unit: 's'
+	}
+
+	if (hasHours === false) return {
+		toString: () => `${ cleanZeros(duration.substr(3)) }m`,
+		value: cleanZeros(duration.substr(3)),
+		unit: 'm'
+	}
+
+	return {
+		toString: () => `${ cleanZeros(duration) }h`,
+		value: cleanZeros(duration),
+		unit: 'h'
+	}
 
 }
