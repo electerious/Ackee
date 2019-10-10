cdddca2d80dc71f765aa6e0899fbed9f835d6b44
Tobias Reich <tobias.reich.ich@gmail.com>
2019-10-10 09:37:50 +0200

    Adjust interval

--- src/constants/durations.js
+++ src/constants/durations.js
@@ -2,7 +2,7 @@ const { hour } = require('../utils/times')
 
 // Constants will be shared between client and server.
 // They will be used as values in the DOM and in the URL of the views calls.
-const DURATIONS_INTERVAL = 10000
+const DURATIONS_INTERVAL = 15000
 const DURATIONS_LIMIT = hour / 2
 // const DURATIONS_TYPE_AVERAGE = 'average'
 const DURATIONS_TYPE_DETAILED = 'detailed'
