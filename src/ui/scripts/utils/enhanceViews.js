075f5134f161991cd461a9ff9251bcd92b3d4a00
Tobias Reich <tobias.reich.ich@gmail.com>
2019-10-13 14:04:47 +0200

    Change name

--- src/ui/scripts/utils/enhanceViews.js
+++ src/ui/scripts/utils/enhanceViews.js
@@ -1,16 +1,16 @@
 import createArray from '../utils/createArray'
 import dateWithOffset from '../../../utils/dateWithOffset'
 
-export default (chartViews, length) => createArray(length).map((_, index) => {
+export default (views, length) => createArray(length).map((_, index) => {
 
 	const date = dateWithOffset(index * -1)
 
 	// Find a view that matches the date
-	const view = chartViews.find((chartView) => {
+	const view = views.find((view) => {
 
-		const isDay = chartView.data.id.day === date.getDate()
-		const isMonth = chartView.data.id.month === date.getMonth() + 1
-		const isYear = chartView.data.id.year === date.getFullYear()
+		const isDay = view.data.id.day === date.getDate()
+		const isMonth = view.data.id.month === date.getMonth() + 1
+		const isYear = view.data.id.year === date.getFullYear()
 
 		return isDay === true && isMonth === true && isYear === true
 
