93ca0b00670b1382095e3894b9bb511d952796cb
Tobias Reich <tobias.reich.ich@gmail.com>
2020-02-24 20:26:47 +0100

    Fix aggregation for total views

--- src/aggregations/aggregateDailyViews.js
+++ src/aggregations/aggregateDailyViews.js
@@ -1,41 +1,48 @@
 'use strict'
 
-module.exports = (id, unique) => [
-	{
-		$match: {
-			clientId: unique === true ? {
-				$exists: true,
-				$ne: null
-			} : undefined,
-			domainId: id
-		}
-	},
-	{
-		$group: {
-			_id: {
-				day: {
-					$dayOfMonth: '$created'
-				},
-				month: {
-					$month: '$created'
-				},
-				year: {
-					$year: '$created'
-				}
-			},
-			count: {
-				$sum: 1
+module.exports = (id, unique) => {
+
+	const aggregation = [
+		{
+			$match: {
+				domainId: id
 			}
+		},
+		{
+			$group: {
+				_id: {
+					day: {
+						$dayOfMonth: '$created'
+					},
+					month: {
+						$month: '$created'
+					},
+					year: {
+						$year: '$created'
+					}
+				},
+				count: {
+					$sum: 1
+				}
+			}
+		},
+		{
+			$sort: {
+				'_id.year': -1,
+				'_id.month': -1,
+				'_id.day': -1
+			}
+		},
+		{
+			$limit: 14
 		}
-	},
-	{
-		$sort: {
-			'_id.year': -1,
-			'_id.month': -1,
-			'_id.day': -1
-		}
-	},
-	{
-		$limit: 14
+	]
+
+	if (unique === true) aggregation[0].$match.clientId = {
+		$exists: true,
+		$ne: null
 	}
-]
+
+	return aggregation
+
+}
--- src/aggregations/aggregateYearlyViews.js
+++ src/aggregations/aggregateYearlyViews.js
@@ -1,33 +1,40 @@
 'use strict'
 
-module.exports = (id, unique) => [
-	{
-		$match: {
-			clientId: unique === true ? {
-				$exists: true,
-				$ne: null
-			} : undefined,
-			domainId: id
-		}
-	},
-	{
-		$group: {
-			_id: {
-				year: {
-					$year: '$created'
-				}
-			},
-			count: {
-				$sum: 1
+module.exports = (id, unique) => {
+
+	const aggregation = [
+		{
+			$match: {
+				domainId: id
 			}
+		},
+		{
+			$group: {
+				_id: {
+					year: {
+						$year: '$created'
+					}
+				},
+				count: {
+					$sum: 1
+				}
+			}
+		},
+		{
+			$sort: {
+				'_id.day': -1
+			}
+		},
+		{
+			$limit: 14
 		}
-	},
-	{
-		$sort: {
-			'_id.day': -1
-		}
-	},
-	{
-		$limit: 14
+	]
+
+	if (unique === true) aggregation[0].$match.clientId = {
+		$exists: true,
+		$ne: null
 	}
-]
+
+	return aggregation
+
+}
