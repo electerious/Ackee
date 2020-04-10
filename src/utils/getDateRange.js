983d083e04626fecf0edf70a9f0bb5ad971f3c8a
Tobias Reich <tobias.reich.ich@gmail.com>
2020-04-10 12:12:23 +0200

    Small code style adjustments

--- src/utils/getDateRange.js
+++ src/utils/getDateRange.js
@@ -6,14 +6,16 @@ const zeroDate = require('./zeroDate')
 const { ALL_TIME, LAST_7_DAYS, LAST_30_DAYS } = require('../constants/dateRange')
 
 module.exports = (dateRange) => {
+
 	switch (dateRange) {
-		case ALL_TIME.value:
-			return null
-		case LAST_30_DAYS.value:
-			return { $gte: subDays(zeroDate(), 29) }
 		case LAST_7_DAYS.value:
 			return { $gte: subDays(zeroDate(), 6) }
+		case LAST_30_DAYS.value:
+			return { $gte: subDays(zeroDate(), 29) }
+		case ALL_TIME.value:
+			return null
 		default:
 			return null
 	}
+
 }
