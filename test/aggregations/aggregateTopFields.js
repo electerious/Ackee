9c568d55fae8551a40e8d203b21848dfcd47890e
Tobias Reich <tobias.reich.ich@gmail.com>
2020-04-10 17:32:27 +0200

    Update deps

--- test/aggregations/aggregateTopFields.js
+++ test/aggregations/aggregateTopFields.js
@@ -1,7 +1,7 @@
 'use strict'
 
 const test = require('ava')
-const uuid = require('uuid/v4')
+const uuid = require('uuid').v4
 
 const aggregateTopFields = require('../../src/aggregations/aggregateTopFields')
 
