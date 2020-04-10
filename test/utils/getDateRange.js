25b90e5d6b26dfaac346f72eb2f64763ac8d51c6
Romain Cascino <contact@romaincascino.com>
2020-04-10 12:00:17 +0200

    Add test for getDateRange

---
+++ test/utils/getDateRange.js
@@ -0,0 +1,51 @@
+'use strict'
+
+const test = require('ava')
+
+const { subDays } = require('date-fns')
+
+const zeroDate = require('../../src/utils/zeroDate')
+const getDateRange = require('../../src/utils/getDateRange')
+const {
+	ALL_TIME,
+	LAST_7_DAYS,
+	LAST_30_DAYS
+} = require('../../src/constants/dateRange')
+
+test('return correct value for LAST_7_DAYS value', async (t) => {
+
+	const result = getDateRange(LAST_7_DAYS.value)
+
+	t.deepEqual(result, { $gte: subDays(zeroDate(), 6) })
+
+})
+
+test('return correct value for LAST_30_DAYS value', async (t) => {
+
+	const result = getDateRange(LAST_30_DAYS.value)
+
+	t.deepEqual(result, { $gte: subDays(zeroDate(), 29) })
+
+})
+
+test('return null for ALL_TIME value', async (t) => {
+
+	const result = getDateRange(ALL_TIME.value)
+
+	t.is(result, null)
+
+})
+
+
+test('return null for other value', async (t) => {
+
+	const resultString = getDateRange('test')
+	const resultNull = getDateRange(null)
+	const resultUndefined = getDateRange(undefined)
+
+	t.is(resultString, null)
+	t.is(resultNull, null)
+	t.is(resultUndefined, null)
+
+})
+
