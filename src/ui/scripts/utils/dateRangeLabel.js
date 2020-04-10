524b573820bc12e443f8b0d3b176bd13e994b01d
Tobias Reich <tobias.reich.ich@gmail.com>
2020-04-10 13:41:00 +0200

    Rename variable

--- src/ui/scripts/utils/dateRangeLabel.js
+++ src/ui/scripts/utils/dateRangeLabel.js
@@ -1,7 +1,7 @@
 import { LAST_7_DAYS, LAST_30_DAYS, ALL_TIME } from '../../../constants/dateRange'
 
-export default (dateRange) => {
+export default (dateRangeValue) => {
 
-	return [ LAST_7_DAYS, LAST_30_DAYS, ALL_TIME ].find((range) => range.value === dateRange).label
+	return [ LAST_7_DAYS, LAST_30_DAYS, ALL_TIME ].find((range) => range.value === dateRangeValue).label
 
 }
