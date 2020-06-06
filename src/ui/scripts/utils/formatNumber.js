86efa9a163ab6de7e1a4e04c6ad811cacadd86bc
Tobias Reich <tobias.reich.ich@gmail.com>
2020-06-06 21:57:50 +0200

    Change format of numbers

--- src/ui/scripts/utils/formatNumber.js
+++ src/ui/scripts/utils/formatNumber.js
@@ -2,6 +2,7 @@ import humanNumber from 'human-number'
 
 export default (num) => {
 
-	return humanNumber(num)
+	return humanNumber(num, (n) => Number.parseFloat(n).toFixed(1))
+		.replace('.0', '')
 
 }
