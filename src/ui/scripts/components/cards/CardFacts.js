99c1959794d8ed18ded52b0c41c3c52237afc9e5
Tobias Reich <tobias.reich.ich@gmail.com>
2020-06-07 14:55:34 +0200

    Restructure facts presentation

--- src/ui/scripts/components/cards/CardFacts.js
+++ src/ui/scripts/components/cards/CardFacts.js
@@ -1,4 +1,4 @@
-import { createElement as h, Fragment } from 'react'
+import { createElement as h } from 'react'
 import PropTypes from 'prop-types'
 
 import formatDuration from '../../utils/formatDuration'
@@ -12,7 +12,7 @@ import PresentationValueText from '../presentations/PresentationValueText'
 const Presentation = (props) => {
 
 	return (
-		h(Fragment, {},
+		h('div', { className: 'card__inner' },
 			h(Headline, {
 				type: 'h2',
 				size: 'small'
@@ -36,55 +36,43 @@ const CardFacts = (props) => {
 			className: 'card card--wide'
 		},
 			h('div', { className: 'card__facts' },
-				h('div', { className: 'card__inner' },
-					h(Presentation, {
-						headline: 'Active visitors',
-						value: props.items.views_active,
-						text: pluralize([ 'visitors', 'visitor', 'visitors' ], props.items.views_active)
-					})
-				),
+				h(Presentation, {
+					headline: 'Active visitors',
+					value: props.items.views_active,
+					text: pluralize([ 'visitors', 'visitor', 'visitors' ], props.items.views_active)
+				}),
 				h('div', { className: 'card__separator card__separator--vertical' }),
-				h('div', { className: 'card__inner' },
-					h(Presentation, {
-						headline: 'Average views',
-						value: formatNumber(props.items.views_average),
-						text: 'per day'
-					})
-				),
+				h(Presentation, {
+					headline: 'Average views',
+					value: formatNumber(props.items.views_average),
+					text: 'per day'
+				}),
 				h('div', { className: 'card__separator card__separator--vertical' }),
-				h('div', { className: 'card__inner' },
-					h(Presentation, {
-						headline: 'Average duration',
-						value: formatDuration(props.items.durations_average).value,
-						text: formatDuration(props.items.durations_average).unit
-					})
-				)
+				h(Presentation, {
+					headline: 'Average duration',
+					value: formatDuration(props.items.durations_average).value,
+					text: formatDuration(props.items.durations_average).unit
+				})
 			),
 			h('div', { className: 'card__separator card__separator--horizontal' }),
 			h('div', { className: 'card__facts' },
-				h('div', { className: 'card__inner' },
-					h(Presentation, {
-						headline: 'Views today',
-						value: formatNumber(props.items.views_today),
-						text: pluralize([ 'views', 'view', 'views' ], props.items.views_today)
-					})
-				),
+				h(Presentation, {
+					headline: 'Views today',
+					value: formatNumber(props.items.views_today),
+					text: pluralize([ 'views', 'view', 'views' ], props.items.views_today)
+				}),
 				h('div', { className: 'card__separator card__separator--vertical' }),
-				h('div', { className: 'card__inner' },
-					h(Presentation, {
-						headline: 'Views this month',
-						value: formatNumber(props.items.views_month),
-						text: pluralize([ 'views', 'view', 'views' ], props.items.views_month)
-					})
-				),
+				h(Presentation, {
+					headline: 'Views this month',
+					value: formatNumber(props.items.views_month),
+					text: pluralize([ 'views', 'view', 'views' ], props.items.views_month)
+				}),
 				h('div', { className: 'card__separator card__separator--vertical' }),
-				h('div', { className: 'card__inner' },
-					h(Presentation, {
-						headline: 'Views this year',
-						value: formatNumber(props.items.views_year),
-						text: pluralize([ 'views', 'view', 'views' ], props.items.views_year)
-					})
-				)
+				h(Presentation, {
+					headline: 'Views this year',
+					value: formatNumber(props.items.views_year),
+					text: pluralize([ 'views', 'view', 'views' ], props.items.views_year)
+				})
 			)
 		)
 	)
