7ee3ca59db7596b21f7b3689e98b0cc1555fec42
Tobias Reich <tobias.reich.ich@gmail.com>
2020-06-07 17:02:10 +0200

    Ignore inactive visitors

--- src/aggregations/aggregateViewsActive.js
+++ src/aggregations/aggregateViewsActive.js
@@ -1,8 +1,9 @@
 'use strict'
 
-const { subSeconds } = require('date-fns')
+const { subSeconds, subMilliseconds } = require('date-fns')
 
 const matchDomainId = require('../stages/matchDomainId')
+const { DURATIONS_LIMIT } = require('../constants/durations')
 
 module.exports = (id) => {
 
@@ -12,6 +13,10 @@ module.exports = (id) => {
 			$count: 'count'
 		}
 	]
+
+	aggregation[0].$match.created = {
+		$gte: subMilliseconds(new Date(), DURATIONS_LIMIT)
+	}
 
 	aggregation[0].$match.updated = {
 		$gte: subSeconds(new Date(), 30)
