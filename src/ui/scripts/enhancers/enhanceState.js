7319dac55c144d27ad15bdaf84c9959930e93c2b
Tobias Reich <tobias.reich.ich@gmail.com>
2020-08-02 18:38:14 +0200

    Overview errors and fetching for all domains

--- src/ui/scripts/enhancers/enhanceState.js
+++ src/ui/scripts/enhancers/enhanceState.js
@@ -1,8 +1,11 @@
+import { ALL_DOMAINS } from '../actions/overview'
 import isDefined from '../../../utils/isDefined'
+import * as selectOverviewValue from '../selectors/selectOverviewValue'
 
 export default (state) => {
 
 	const fetching = (
+		selectOverviewValue.withoutType(state, ALL_DOMAINS).fetching === true ||
 		Object.values(state.overview.value).some((value) => value.fetching) === true ||
 		state.views.fetching === true ||
 		state.pages.fetching === true ||
@@ -18,6 +21,7 @@ export default (state) => {
 	)
 
 	const errors = [
+		selectOverviewValue.withoutType(state, ALL_DOMAINS).error,
 		...Object.values(state.overview.value).map((value) => value.error),
 		state.views.error,
 		state.pages.error,
