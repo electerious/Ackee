ce0a418f4a89d3cd2f8863017b14c0f5048c14db
Tobias Reich <tobias.reich.ich@gmail.com>
2020-09-08 16:47:31 +0200

    Import specific files to avoid "circular dependency" warning

--- src/types/index.js
+++ src/types/index.js
@@ -1,7 +1,15 @@
 'use strict'
 
+const { resolve } = require('path')
 const { loadFilesSync, mergeTypeDefs } = require('graphql-tools')
 
-const typesArray = loadFilesSync(__dirname)
+const typesArray = loadFilesSync([
+	resolve(__dirname, './domains.graphql'),
+	resolve(__dirname, './facts.graphql'),
+	resolve(__dirname, './miscellaneous.graphql'),
+	resolve(__dirname, './records.graphql'),
+	resolve(__dirname, './statistics.graphql'),
+	resolve(__dirname, './tokens.graphql')
+])
 
 module.exports = mergeTypeDefs(typesArray, { all: true })
