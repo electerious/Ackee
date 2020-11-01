782f2a22c2acfdac879fddb17ae5c405c56990f3
Tobias Reich <tobias.reich.ich@gmail.com>
2020-11-01 16:40:16 +0100

    Better context creation

--- src/utils/createContext.js
+++ src/utils/createContext.js
@@ -2,19 +2,23 @@ const isDemoMode = require('./isDemoMode')
 const isAuthenticated = require('./isAuthenticated')
 const createDate = require('./createDate')
 
-const createServerlessContext = async (integrationContext) => ({
-	isDemoMode,
-	isAuthenticated: await isAuthenticated(integrationContext.event.headers['authorization']),
-	dateDetails: createDate(integrationContext.event.headers['time-zone']),
-	req: integrationContext.req
-})
+const createServerlessContext = async (integrationContext) => {
+	return createContext(integrationContext.event.headers, integrationContext.req)
+}
 
-const createMicroContext = async (integrationContext) => ({
-	isDemoMode,
-	isAuthenticated: await isAuthenticated(integrationContext.req.headers['authorization']),
-	dateDetails: createDate(integrationContext.req.headers['time-zone']),
-	req: integrationContext.req
-})
+const createMicroContext = async (integrationContext) => {
+	return createContext(integrationContext.req.headers, integrationContext.req)
+}
+
+const createContext = async (req, headers) => {
+	return {
+		isDemoMode,
+		isAuthenticated: await isAuthenticated(headers['authorization']),
+		dateDetails: createDate(headers['time-zone']),
+		headers,
+		req
+	}
+}
 
 module.exports = {
 	createServerlessContext,
