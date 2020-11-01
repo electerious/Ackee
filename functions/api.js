3afc0ec48a528ed92ba365ff053c125351111945
Spencer Elliott <me@elliottsj.com>
2020-11-01 16:31:14 -0500

    Vercel + Netlify serverless functions

---
+++ functions/api.js
@@ -0,0 +1,9 @@
+'use strict'
+
+/**
+ * A serverless function handler for the '/api' route, for use with Netlify.
+ *
+ * See:
+ *  - https://docs.netlify.com/functions/overview/
+ */
+exports.handler = require('../src/serverless').handler
