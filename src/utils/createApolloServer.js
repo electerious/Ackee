be0b46ecbb56997d0a58e007443c712ead346983
Tobias Reich <tobias.reich.ich@gmail.com>
2020-11-08 12:39:24 +0100

    Fix path

--- src/utils/createApolloServer.js
+++ src/utils/createApolloServer.js
@@ -3,7 +3,7 @@
 const { UnsignedIntResolver, UnsignedIntTypeDefinition, DateTimeResolver, DateTimeTypeDefinition } = require('graphql-scalars')
 
 const isDemoMode = require('./isDemoMode')
-const isDevelopmentMode = require('./utils/isDevelopmentMode')
+const isDevelopmentMode = require('./isDevelopmentMode')
 
 module.exports = (ApolloServer, opts) => new ApolloServer({
 	introspection: isDemoMode === true || isDevelopmentMode === true,
