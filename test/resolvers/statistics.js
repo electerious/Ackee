f189c6df74483854852ff046f6dd5a60a667c667
Tobias Reich <tobias.reich.ich@gmail.com>
2020-11-08 19:52:14 +0100

    Add tests for languages

--- test/resolvers/statistics.js
+++ test/resolvers/statistics.js
@@ -13,12 +13,6 @@ test.beforeEach(fillDatabase)
 test.afterEach.always(cleanupDatabase)
 test.after.always(disconnectFromDatabase)
 
-// languages(sorting: $sorting, range: $range) {
-// 	id
-// 	count
-// 	created
-// }
-
 const getStats = async (t, fragment) => {
 
 	const body = {
@@ -98,6 +92,14 @@ const browsersFragment = (sorting, type, range) => `
 
 const sizesFragment = (sorting, type, range) => `
 	sizes(sorting: ${ sorting }, type: ${ type }, range: ${ range }) {
+		id
+		count
+		created
+	}
+`
+
+const languagesFragment = (sorting, range) => `
+	languages(sorting: ${ sorting }, range: ${ range }) {
 		id
 		count
 		created
@@ -461,128 +463,29 @@ test('fetch NEW, SCREEN_RESOLUTION and LAST_6_MONTHS sizes', async (t) => {
 
 })
 
-// test('fetch TOP statistics', async (t) => {
+test('fetch TOP and LAST_6_MONTHS languages', async (t) => {
 
-// 	const domainStatistics = await getStats(t, 'TOP', 'NO_VERSION', 'NO_VERSION', 'NO_MODEL', 'BROWSER_RESOLUTION')
+	const statistics = await getStats(t, languagesFragment('TOP', 'LAST_6_MONTHS'))
 
-// 	t.is(domainStatistics.views.length, 14)
-// 	t.is(domainStatistics.views[0].count, 1)
-// 	t.is(domainStatistics.pages.length, 1)
-// 	t.is(domainStatistics.pages[0].id, 'https://example.com/')
-// 	t.is(domainStatistics.referrers.length, 1)
-// 	t.is(domainStatistics.referrers[0].id, 'https://google.com/')
-// 	t.is(domainStatistics.durations.length, 14)
-// 	t.is(domainStatistics.durations[0].count, 60 * 1000)
-// 	t.is(domainStatistics.systems.length, 1)
-// 	t.is(domainStatistics.systems[0].id, 'iOS')
-// 	t.is(domainStatistics.devices.length, 1)
-// 	t.is(domainStatistics.devices[0].id, 'Apple')
-// 	t.is(domainStatistics.browsers.length, 1)
-// 	t.is(domainStatistics.browsers[0].id, 'Safari')
-// 	t.is(domainStatistics.sizes.length, 1)
-// 	t.is(domainStatistics.sizes[0].id, '414px x 719px')
-// 	t.is(domainStatistics.languages.length, 1)
-// 	t.is(domainStatistics.languages[0].id, 'English')
+	t.is(statistics.languages.length, 1)
+	t.is(statistics.languages[0].id, 'English')
 
-// })
+})
 
-// test('fetch RECENT statistics', async (t) => {
+test('fetch RECENT and LAST_6_MONTHS languages', async (t) => {
 
-// 	const domainStatistics = await getStats(t, 'RECENT', 'WITH_VERSION', 'WITH_VERSION', 'WITH_MODEL', 'BROWSER_WIDTH')
+	const statistics = await getStats(t, languagesFragment('RECENT', 'LAST_6_MONTHS'))
 
-// 	t.is(domainStatistics.views.length, 14)
-// 	t.is(domainStatistics.views[0].count, 1)
-// 	t.is(domainStatistics.pages.length, 14)
-// 	t.is(domainStatistics.pages[0].id, 'https://example.com/')
-// 	t.is(domainStatistics.referrers.length, 14)
-// 	t.is(domainStatistics.referrers[0].id, 'https://google.com/')
-// 	t.is(domainStatistics.durations.length, 14)
-// 	t.is(domainStatistics.durations[0].count, 60 * 1000)
-// 	t.is(domainStatistics.systems.length, 14)
-// 	t.is(domainStatistics.systems[0].id, 'iOS 14.0')
-// 	t.is(domainStatistics.systems[8].id, 'iOS 13.0')
-// 	t.is(domainStatistics.devices.length, 14)
-// 	t.is(domainStatistics.devices[0].id, 'Apple iPhone')
-// 	t.is(domainStatistics.browsers.length, 14)
-// 	t.is(domainStatistics.browsers[0].id, 'Safari 14.0')
-// 	t.is(domainStatistics.browsers[8].id, 'Safari 13.0')
-// 	t.is(domainStatistics.sizes.length, 14)
-// 	t.is(domainStatistics.sizes[0].id, '414px')
-// 	t.is(domainStatistics.languages.length, 14)
-// 	t.is(domainStatistics.languages[0].id, 'English')
+	t.is(statistics.languages.length, 14)
+	t.is(statistics.languages[0].id, 'English')
 
-// })
+})
 
-// test('fetch NEW statistics', async (t) => {
+test('fetch NEW and LAST_6_MONTHS languages', async (t) => {
 
-// 	const domainStatistics = await getStats(t, 'NEW', 'WITH_VERSION', 'WITH_VERSION', 'WITH_MODEL', 'BROWSER_HEIGHT')
+	const statistics = await getStats(t, languagesFragment('NEW', 'LAST_6_MONTHS'))
 
-// 	t.is(domainStatistics.views.length, 14)
-// 	t.is(domainStatistics.views[0].count, 1)
-// 	t.is(domainStatistics.pages.length, 1)
-// 	t.is(domainStatistics.pages[0].id, 'https://example.com/')
-// 	t.is(domainStatistics.referrers.length, 1)
-// 	t.is(domainStatistics.referrers[0].id, 'https://google.com/')
-// 	t.is(domainStatistics.durations.length, 14)
-// 	t.is(domainStatistics.durations[0].count, 60 * 1000)
-// 	t.is(domainStatistics.systems.length, 2)
-// 	t.is(domainStatistics.systems[0].id, 'iOS 14.0')
-// 	t.is(domainStatistics.systems[1].id, 'iOS 13.0')
-// 	t.is(domainStatistics.devices.length, 1)
-// 	t.is(domainStatistics.devices[0].id, 'Apple iPhone')
-// 	t.is(domainStatistics.browsers.length, 2)
-// 	t.is(domainStatistics.browsers[0].id, 'Safari 14.0')
-// 	t.is(domainStatistics.browsers[1].id, 'Safari 13.0')
-// 	t.is(domainStatistics.sizes.length, 1)
-// 	t.is(domainStatistics.sizes[0].id, '719px')
-// 	t.is(domainStatistics.languages.length, 1)
-// 	t.is(domainStatistics.languages[0].id, 'English')
+	t.is(statistics.languages.length, 1)
+	t.is(statistics.languages[0].id, 'English')
 
-// })
-
-// test('fetch TOP screen statistics', async (t) => {
-
-// 	const domainStatistics = await getStats(t, 'TOP', 'WITH_VERSION', 'WITH_VERSION', 'NO_MODEL', 'SCREEN_RESOLUTION')
-
-// 	t.is(domainStatistics.systems.length, 1)
-// 	t.is(domainStatistics.systems[0].id, 'iOS 14.0')
-// 	t.is(domainStatistics.devices.length, 1)
-// 	t.is(domainStatistics.devices[0].id, 'Apple')
-// 	t.is(domainStatistics.browsers.length, 1)
-// 	t.is(domainStatistics.browsers[0].id, 'Safari 14.0')
-// 	t.is(domainStatistics.sizes.length, 1)
-// 	t.is(domainStatistics.sizes[0].id, '414px x 896px')
-
-// })
-
-// test('fetch RECENT screen statistics', async (t) => {
-
-// 	const domainStatistics = await getStats(t, 'RECENT', 'NO_VERSION', 'NO_VERSION', 'WITH_MODEL', 'SCREEN_WIDTH')
-
-// 	t.is(domainStatistics.systems.length, 14)
-// 	t.is(domainStatistics.systems[0].id, 'iOS')
-// 	t.is(domainStatistics.systems[8].id, 'iOS')
-// 	t.is(domainStatistics.devices.length, 14)
-// 	t.is(domainStatistics.devices[0].id, 'Apple iPhone')
-// 	t.is(domainStatistics.browsers.length, 14)
-// 	t.is(domainStatistics.browsers[0].id, 'Safari')
-// 	t.is(domainStatistics.browsers[8].id, 'Safari')
-// 	t.is(domainStatistics.sizes.length, 14)
-// 	t.is(domainStatistics.sizes[0].id, '414px')
-
-// })
-
-// test('fetch NEW screen statistics', async (t) => {
-
-// 	const domainStatistics = await getStats(t, 'NEW', 'NO_VERSION', 'NO_VERSION', 'WITH_MODEL', 'SCREEN_HEIGHT')
-
-// 	t.is(domainStatistics.systems.length, 1)
-// 	t.is(domainStatistics.systems[0].id, 'iOS')
-// 	t.is(domainStatistics.devices.length, 1)
-// 	t.is(domainStatistics.devices[0].id, 'Apple iPhone')
-// 	t.is(domainStatistics.browsers.length, 1)
-// 	t.is(domainStatistics.browsers[0].id, 'Safari')
-// 	t.is(domainStatistics.sizes.length, 1)
-// 	t.is(domainStatistics.sizes[0].id, '896px')
-
-// })
+})
