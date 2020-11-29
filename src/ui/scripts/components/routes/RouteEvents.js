16ed1831b6bfb638df3e5664de3d2e5ba84d6e51
Tobias Reich <tobias.reich.ich@gmail.com>
2020-11-29 19:22:14 +0100

    Reset events card for later

--- src/ui/scripts/components/routes/RouteEvents.js
+++ src/ui/scripts/components/routes/RouteEvents.js
@@ -1,40 +1,8 @@
-import { createElement as h, Fragment, useEffect } from 'react'
+import { createElement as h } from 'react'
 
-// import { VIEWS_TYPE_UNIQUE, VIEWS_TYPE_TOTAL } from '../../../../constants/views'
-// import selectViewsValue from '../../selectors/selectViewsValue'
-// import enhanceViews from '../../enhancers/enhanceViews'
-// import mergeViews from '../../utils/mergeViews'
-// import overviewRoute from '../../utils/overviewRoute'
+const RouteEvents = () => {
 
-import CardViews from '../cards/CardViews'
-
-const RouteEvents = (props) => {
-
-	useEffect(() => {
-
-		props.fetchEvents(props)
-
-	}, [])
-
-	return (
-		h(Fragment, {},
-
-			props.events.value.map(
-				(event) => (
-					h(CardViews, {
-						key: event.id,
-						headline: event.title,
-						interval: props.filter.interval,
-						// loading: props.views.fetching,
-						loading: false,
-						// items: enhanceViews(selectViewsValue(props, domain.id).value, 7),
-						items: []
-					})
-				)
-			)
-
-		)
-	)
+	return null
 
 }
 
