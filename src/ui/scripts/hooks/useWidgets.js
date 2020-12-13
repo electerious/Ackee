0189faf485f8e09154079a0d8b9983c412b1ff83
Tobias Reich <tobias.reich.ich@gmail.com>
2020-12-13 14:17:33 +0100

    Refactor useWidgets

--- src/ui/scripts/hooks/useWidgets.js
+++ src/ui/scripts/hooks/useWidgets.js
@@ -4,67 +4,51 @@ import { initialSubState } from '../reducers/widgets'
 
 import CardWidget from '../components/cards/CardWidget'
 
-const defaultWidgetConfig = {
-	WidgetComponent: CardWidget,
-	additionalProps: {}
-}
-
 export default (props, widgetConfigs = []) => {
 
-	const loaders = useMemo(() => {
+	const enhancedWidgetConfigs = useMemo(() => {
 
-		return widgetConfigs.map((widgetConfig) =>
-			widgetConfig.loader
-		)
+		// Set defaults and overwrite with the config passed to the hook
+		return widgetConfigs.map((widgetConfig) => ({
+			key: widgetConfig.loader.id,
+			WidgetComponent: CardWidget,
+			additionalProps: {},
+			...widgetConfig
+		}))
 
 	}, [ widgetConfigs ])
 
 	const enhancedWidgetData = useMemo(() => {
 
-		return loaders.map((loader) => {
-			const widgetId = loader.id
+		// The loader already contains most data required to render the widget.
+		// We therefore don't need to wait until the remaining data is available through the store.
+		return enhancedWidgetConfigs.map(({ loader }) => ({
+			...initialSubState(),
+			// Initialize the value with the correct type by calling the enhancer without parameters.
+			// This ensures that there's always an empty value with the correct type available to render.
+			value: loader.enhancer(),
+			Renderer: loader.Renderer,
+			variables: loader.variables,
+			// Overwrite the constructed data with the data in the store (when available).
+			...(props.widgets.value[loader.id] || {})
+		}))
 
-			// The loader already contains most data required to render the widget.
-			// We therefore don't need to wait until the remaining data is available through the store.
-			return {
-				...initialSubState(),
-				// Initialize the value with the correct type by calling the enhancer without parameters.
-				// This ensures that there's always an empty value with the correct type available to render.
-				value: loader.enhancer(),
-				Renderer: loader.Renderer,
-				variables: loader.variables,
-				// Overwrite the constructed data with the data in the store (when available).
-				...(props.widgets.value[widgetId] || {})
-			}
-		})
-
-	}, [ loaders, props.widgets.value ])
-
-	const enhancedWidgetConfigs = useMemo(() => {
-
-		return loaders.map((loader, index) => {
-			return {
-				...defaultWidgetConfig,
-				...widgetConfigs[index]
-			}
-		})
-
-	}, [ loaders, widgetConfigs ])
+	}, [ enhancedWidgetConfigs, props.widgets.value ])
 
 	useEffect(() => {
 
+		// Fetch widgets in useEffect, because updating the state inside useMemo isn't allowed
+		const loaders = widgetConfigs.map(({ loader }) => loader)
 		props.fetchWidgets(props, loaders)
 
-	}, [ loaders ])
+	}, [ enhancedWidgetConfigs ])
 
-	return loaders.map(
-		(loader, index) => {
-			const widgetId = loader.id
+	return enhancedWidgetConfigs.map(
+		(widgetConfig, index) => {
 			const widgetState = enhancedWidgetData[index]
-			const widgetConfig = enhancedWidgetConfigs[index]
 
 			return h(widgetConfig.WidgetComponent, {
-				key: widgetConfig.key || widgetId,
+				key: widgetConfig.key,
 				widget: widgetState,
 				...widgetConfig.additionalProps
 			})
