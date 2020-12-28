8d54df88d4a48c7c1faf463fac7602ccefe0e6e9
Tobias Reich <tobias.reich.ich@gmail.com>
2020-12-28 14:10:15 +0100

    Adjust expired test

--- test/utils/isExpired.js
+++ test/utils/isExpired.js
@@ -5,52 +5,24 @@ const test = require('ava')
 const { day } = require('../../src/utils/times')
 const isExpired = require('../../src/utils/isExpired')
 
+test('return true when `timestamp` has expired', (t) => {
+
+	const date = new Date()
+	date.setDate(date.getDate() - 2)
+	const timestamp = date.getTime()
+
+	const result = isExpired(timestamp, day)
+
+	t.true(result)
+
+})
+
 test('return false when `timestamp` is valid', (t) => {
 
-	const date = new Date()
-	const timestamp = date.getTime()
+	const timestamp = Date.now()
 
 	const result = isExpired(timestamp, day)
 
 	t.false(result)
-
-})
-
-test('return true when `timestamp` is invalid', (t) => {
-
-	const date = new Date()
-
-	date.setDate(date.getDate() - 1)
-
-	const timestamp = date.getTime()
-
-	const result = isExpired(timestamp, day)
-
-	t.true(result)
-
-})
-
-test('return false when `timestamp` is newer than `ttl`', (t) => {
-
-	const date = new Date()
-	const timestamp = date.getTime()
-
-	const result = isExpired(timestamp, 86400000)
-
-	t.false(result)
-
-})
-
-test('return true when `timestamp` is older than `ttl`', (t) => {
-
-	const date = new Date()
-
-	date.setDate(date.getDate() - 1)
-
-	const timestamp = date.getTime()
-
-	const result = isExpired(timestamp, 0)
-
-	t.true(result)
 
 })
