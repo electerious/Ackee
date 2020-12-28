23240734253679f73b57aa5f93b80bf97d1ca874
Tobias Reich <tobias.reich.ich@gmail.com>
2020-12-28 17:04:46 +0100

    Remove fillDatabase in demo mode
    Replaced with ackee-faker

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ const server = require('./server')
 const signale = require('./utils/signale')
 const config = require('./utils/config')
 const connect = require('./utils/connect')
-const fillDatabase = require('./utils/fillDatabase')
 const stripUrlAuth = require('./utils/stripUrlAuth')
 
 if (config.dbUrl == null) {
@@ -27,23 +26,11 @@ connect(config.dbUrl).then(() => {
 	server.listen(config.port)
 
 	if (config.isDevelopmentMode === true) {
-
 		signale.info('Development mode enabled')
-
 	}
 
 	if (config.isDemoMode === true) {
-
-		const job = fillDatabase(`http://localhost:${ config.port }`)
-		const date = job.nextInvocation()
-
-		const formattedDate = `${ date.getDate() }.${ date.getMonth() }.${ date.getFullYear() }`
-		const formattedTime = `${ date.getHours() }:${ date.getMinutes() }:${ date.getSeconds() }`
-
 		signale.info('Demo mode enabled')
-		signale.info('New records will be added minutely')
-		signale.info(`Next record fill on ${ formattedDate } at ${ formattedTime } o'clock`)
-
 	}
 
 }).catch((err) => {
--- src/utils/sleep.js
+++
@@ -1,3 +0,0 @@
-'use strict'
-
-module.exports = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
