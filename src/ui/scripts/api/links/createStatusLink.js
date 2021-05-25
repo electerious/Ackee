d8a011340e8003415441915e82306a56beaee7fa
Tobias Reich <tobias.reich.ich@gmail.com>
2021-05-25 17:57:42 +0200

    Use custom status reducer

--- src/ui/scripts/api/links/createStatusLink.js
+++ src/ui/scripts/api/links/createStatusLink.js
@@ -1,26 +1,44 @@
-import { useMemo } from 'react'
-import { createNetworkStatusNotifier } from 'react-apollo-network-status'
+import { createNetworkStatusNotifier, ActionTypes } from 'react-apollo-network-status'
+
+const pendingRequestsReducer = (state, action) => {
+	switch (action.type) {
+		case ActionTypes.REQUEST:
+			return state + 1
+		case ActionTypes.ERROR:
+		case ActionTypes.SUCCESS:
+		case ActionTypes.CANCEL:
+			return Math.max(state - 1, 0)
+		default:
+			return state
+	}
+}
+
+const errorsReducer = (state, action) => {
+	switch (action.type) {
+		case ActionTypes.REQUEST:
+			return []
+		case ActionTypes.ERROR:
+			const { networkError } = action.payload
+			return [ networkError ]
+		case ActionTypes.SUCCESS:
+			const { result } = action.payload
+			const hasErrors = result != null && result.errors != null
+			return hasErrors === true ? [ ...result.errors ] : state
+		default:
+			return state
+	}
+}
 
 export default () => {
 
-	const { link, useApolloNetworkStatus } = createNetworkStatusNotifier()
+	const { link, useApolloNetworkStatusReducer } = createNetworkStatusNotifier()
 
 	const useStatus = () => {
-		const status = useApolloNetworkStatus()
-
-		const loading = (status.numPendingQueries + status.numPendingMutations) > 0
-		const errors = useMemo(() => {
-			return [
-				status.queryError?.networkError,
-				status.mutationError?.networkError
-			]
-				.concat(status.queryError?.graphQLErrors)
-				.concat(status.mutationError?.graphQLErrors)
-				.filter(Boolean)
-		}, [ status.queryError, status.mutationError ])
+		const pendingRequests = useApolloNetworkStatusReducer(pendingRequestsReducer, 0)
+		const errors = useApolloNetworkStatusReducer(errorsReducer, [])
 
 		return {
-			loading,
+			loading: pendingRequests > 0,
 			errors
 		}
 	}
