93854c661e69a94bf2253507d29b58afc7b24c6f
Tobias Reich <tobias.reich.ich@gmail.com>
2021-06-12 15:12:07 +0200

    Modal with headline and status

--- src/ui/scripts/components/modals/ModalOverviewViews.js
+++ src/ui/scripts/components/modals/ModalOverviewViews.js
@@ -4,24 +4,36 @@ import PropTypes from 'prop-types'
 import { INTERVALS_DAILY } from '../../../../constants/intervals'
 import { VIEWS_TYPE_UNIQUE } from '../../../../constants/views'
 
+import Headline from '../Headline'
+import Text from '../Text'
+import CurrentStatus from '../CurrentStatus'
 import PresentationCounterList from '../presentations/PresentationCounterList'
 
 import useCombinedViews from '../../api/hooks/views/useCombinedViews'
+import relativeDays from '../../utils/relativeDays'
 import commonModalProps from '../../utils/commonModalProps'
 
 const ModalOverviewViews = (props) => {
-	const { value } = useCombinedViews({
+	const { value, status } = useCombinedViews({
 		interval: INTERVALS_DAILY,
 		type: VIEWS_TYPE_UNIQUE,
 		limit: 14,
 	})
 
-	console.log(value)
-
 	return (
 		h('div', { className: 'card' },
 			h('div', { className: 'card__inner' },
 
+				h(Headline, {
+					type: 'h2',
+					size: 'medium',
+				}, 'Views'),
+				h(Text, {
+					type: 'div',
+					spacing: false,
+				},
+					h(CurrentStatus, status, relativeDays(props.index)),
+				),
 				h(PresentationCounterList, {
 					items: value[props.index],
 				}),
