f86eddce1591dbfae4cc7601bf0f3e18a62b12b3
Tobias Reich <tobias.reich.ich@gmail.com>
2021-07-24 12:05:32 +0200

    Fix error when password is missing

--- src/resolvers/tokens.js
+++ src/resolvers/tokens.js
@@ -17,7 +17,7 @@ module.exports = {
 			const { username, password } = input
 
 			if (config.username == null) throw new KnownError('Ackee username missing in environment')
-			if (config.password == null) throw new KnownError('Ackee username missing in environment')
+			if (config.password == null) throw new KnownError('Ackee password missing in environment')
 
 			if (username !== config.username) throw new KnownError('Username or password incorrect')
 			if (password !== config.password) throw new KnownError('Username or password incorrect')
