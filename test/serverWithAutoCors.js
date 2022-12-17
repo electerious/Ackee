44ea91eadc3e25602be34b7ddcec842cb2724a47
Tobias Reich <tobias.reich.ich@gmail.com>
2022-12-17 19:55:48 +0100

    Combine tests as two mockedEnv/restore calls in one file can cause conflicts

--- test/serverWithAutoCors.js
+++ test/serverWithAutoCors.js
@@ -16,47 +16,33 @@ test.before(connectToDatabase)
 test.after.always(disconnectFromDatabase)
 test.beforeEach(async (t) => {
 	t.context.domain1 = await Domain.create({ title: 'fqdn.example.com' })
-	t.context.domain2 = await Domain.create({ title: 'not-an-fqdn' })
+	t.context.domain2 = await Domain.create({ title: 'No fqdn' })
 })
 test.afterEach.always(async (t) => {
 	await Domain.findOneAndDelete({ id: t.context.domain1.id })
 	await Domain.findOneAndDelete({ id: t.context.domain2.id })
 })
 
-test('return cors headers for domain with fully qualifed domain name', async (t) => {
+test('return cors headers based on fully qualifed domain names', async (t) => {
 	const url = new URL('/api', await base)
-	const origin = 'fqdn.example.com'
 
 	const restore = mockedEnv({
 		ACKEE_AUTO_ORIGIN: 'true',
 	})
 
-	const { headers } = await fetch(url.href, { headers: { Host: origin } })
+	const { headers: fqdnHeaders } = await fetch(url.href, { headers: { Host: 'fqdn.example.com' } })
 
-	console.log(headers)
+	t.is(fqdnHeaders.get('Access-Control-Allow-Origin'), 'fqdn.example.com')
+	t.is(fqdnHeaders.get('Access-Control-Allow-Methods'), 'GET, POST, PATCH, OPTIONS')
+	t.is(fqdnHeaders.get('Access-Control-Allow-Headers'), 'Content-Type, Authorization, Time-Zone')
+	t.is(fqdnHeaders.get('Access-Control-Allow-Credentials'), 'true')
 
-	t.is(headers.get('Access-Control-Allow-Origin'), origin)
-	t.is(headers.get('Access-Control-Allow-Methods'), 'GET, POST, PATCH, OPTIONS')
-	t.is(headers.get('Access-Control-Allow-Headers'), 'Content-Type, Authorization, Time-Zone')
-	t.is(headers.get('Access-Control-Allow-Credentials'), 'true')
+	const { headers: noFqdnHeaders } = await fetch(url.href, { headers: { Host: 'No fqdn' } })
 
-	restore()
-})
-
-test('do not return cors headers for domain that is not a fully qualified domain name', async (t) => {
-	const url = new URL('/api', await base)
-	const origin = 'not-an-fqdn'
-
-	const restore = mockedEnv({
-		ACKEE_AUTO_ORIGIN: 'true',
-	})
-
-	const { headers } = await fetch(url.href, { headers: { Host: origin } })
-
-	t.is(headers.get('Access-Control-Allow-Origin'), null)
-	t.is(headers.get('Access-Control-Allow-Methods'), null)
-	t.is(headers.get('Access-Control-Allow-Headers'), null)
-	t.is(headers.get('Access-Control-Allow-Credentials'), null)
+	t.is(noFqdnHeaders.get('Access-Control-Allow-Origin'), null)
+	t.is(noFqdnHeaders.get('Access-Control-Allow-Methods'), null)
+	t.is(noFqdnHeaders.get('Access-Control-Allow-Headers'), null)
+	t.is(noFqdnHeaders.get('Access-Control-Allow-Credentials'), null)
 
 	restore()
 })
